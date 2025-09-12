af8436f29785c02425d91e853d39e7ac0ddb80cb
松坂有希 <becky.balde@gmail.com>
2025-09-12 18:16:06 +0800

    Update config.ts
    update:twikoo支持语言配置

--- src/config.ts
+++ src/config.ts
@@ -277,6 +277,7 @@ export const commentConfig: CommentConfig = {
 	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	twikoo: {
 		envId: "https://twikoo.vercel.app",
+		lang: "en", // 设置 Twikoo 评论系统语言为英文
 	},
 };
 
