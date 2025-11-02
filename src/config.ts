83d0192219da9e75f785a783bd8d0be404ba5132
Xayah-Hina <ycxiang14sep@gmail.com>
2025-11-02 18:52:56 +0800

    initial commit

--- src/config.ts
+++ src/config.ts
@@ -197,9 +197,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/Hatsune.Miku.full.1961310.JPG", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "Xayah Hina",
-	bio: "Graphics Enthusiast",
+	bio: "Graphics & Music",
 	typewriter: {
 		enable: false, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
---
+++ src/content/posts/physical-fused-kernel.md
@@ -0,0 +1,10 @@
+---
+title: Write Physical Fully-fused CUDA Kernel
+published: 2025-11-02
+description: 'physical fully fused kernal'
+image: ''
+tags: [PINF]
+category: 'Graphics'
+draft: false 
+lang: 'en'
+---
