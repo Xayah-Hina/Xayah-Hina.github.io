3c16bba106d27399f836bdf2c318e519af0ee9ec
Xayah-Hina <ycxiang14sep@gmail.com>
2026-07-22 05:22:30 +0800

    Journal: update monthly review 2026-07

--- journals/monthly/2026.js
+++ journals/monthly/2026.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  "2026-07": {
+    "summary": "Summary",
+    "highlights": [
+      "Highligt1",
+      "Highligt2",
+      "Highligt3"
+    ],
+    "next": "Next Month",
+    "reportImage": null,
+    "updatedAt": "2026-07-22T05:22:29+08:00"
+  }
+};
