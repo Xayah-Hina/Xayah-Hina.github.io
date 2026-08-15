65807af53b28276905e1eec0cdf3a6b44449a33b
Xayah <ycxiang14sep@gmail.com>
2026-08-16 02:08:42 +0800

    Journal: update 20260816-020819-7462

--- journals/2026.js
+++ journals/2026.js
@@ -4,7 +4,11 @@ export default [
     "publishedAt": "2026-08-16T02:08:19+08:00",
     "content": "明天要好好计划一下工作。Token还剩34%，20号中午才会刷新，剩下四天半要好好整理一下了不要大动干戈加feature了。剩下token额度全给review吧。对了，Spectra   2.0 Documentation也可以开始动笔了。\n\n另外，下半月开始每天要给论文阅读定kpi了，还是要精读要有成果。静下心来一点一点做事。",
     "images": [],
-    "relatedWriting": null
+    "relatedWriting": {
+      "id": "20260801-130218",
+      "title": "Spectra 2.0 Documentation"
+    },
+    "updatedAt": "2026-08-16T02:08:40+08:00"
   },
   {
     "id": "20260816-020319-6928",
