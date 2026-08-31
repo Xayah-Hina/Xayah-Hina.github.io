a9dbc71c3d688be7a8b872b9971df3f4153760b1
Xayah <ycxiang14sep@gmail.com>
2026-08-31 23:29:22 +0800

    Journal: update monthly note 2026-08

--- journals/monthly/2026.js
+++ journals/monthly/2026.js
@@ -3,5 +3,10 @@ export default {
     "note": "本月小结：\n1. 初步做完了Differentiable  Solver的框架 https://github.com/Xayah-Graphics/diff-solver\n2. 上线了 xayah.me\n3. 重构了spectra，现在vulkan同时负责光栅和光追了 https://github.com/Xayah-Graphics/spectra\n4. 刷了leetcode 1~29\n5. 编谱/练习春日影 48 / 152小节",
     "reportImage": null,
     "updatedAt": "2026-08-01T08:38:16+08:00"
+  },
+  "2026-08": {
+    "note": "本月小结：\n\n1. spectra开发完成，进入稳定阶段。\n2. physica支持布料/烟雾/3D、4D重建/生成式模型。\n3. 入门了Flow Matching。",
+    "reportImage": null,
+    "updatedAt": "2026-08-31T23:29:21+08:00"
   }
 };
